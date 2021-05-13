1a73ca98a89213954af7dd76169c7242b6f22fef
Wojciech Lawren <wojciech.lawren@gmail.com>
2021-05-13 11:56:17 +0200

    asmath

--- asmath.c
+++ asmath.c
@@ -4,35 +4,33 @@
 /* v = a + b */
 uint64_t add(const uint64_t a, const uint64_t b, uint8_t *c)
 {
-    register uint64_t v asm("rax");
+    register uint64_t v = a;
 
-    asm volatile("addq %2, %1\n\t"
-                 "setc (%3)\n\t"
-                 "movq %1, %0"
-                 : "=r"(v)
-                 : "r"(a), "r"(b), "r"(c)
+    asm volatile("addq %1, %0\n\t"
+                 "setc (%2)"
+                 : "+r"(v)
+                 : "r"(b), "r"(c)
                  : "cc");
     return v;
 }
 
 /* v = a + b + CF */
-uint64_t adc(const uint64_t a, const uint64_t b, uint8_t *c)
+uint64_t adcx(const uint64_t a, const uint64_t b, uint8_t *c)
 {
-    register uint64_t v asm("rax");
+    register uint64_t v = a;
 
-    asm volatile("adcxq %2, %1\n\t"
-                 "setc (%3)\n\t"
-                 "movq %1, %0"
-                 : "=r"(v)
-                 : "r"(a), "r"(b), "r"(c)
+    asm volatile("adcxq %1, %0\n\t"
+                 "setc (%2)"
+                 : "+r"(v)
+                 : "r"(b), "r"(c)
                  : "cc");
     return v;
 }
 
 /* r[2] = a * b */
-void mul(uint64_t *r, const uint64_t a, const uint64_t b)
+void mulx(uint64_t *r, const uint64_t a, const uint64_t b)
 {
-    register uint64_t v1 asm("rsi") = a;
+    register uint64_t v1 = a;
     register uint64_t v2 asm("rdx") = b;
 
     asm volatile("mulxq %0, %1, %0" : "+r"(v1), "+r"(v2));
@@ -43,26 +41,38 @@ void mul(uint64_t *r, const uint64_t a, const uint64_t b)
 /* v = a - b */
 uint64_t sub(const uint64_t a, const uint64_t b, uint8_t *c)
 {
-    register uint64_t v asm("rax");
+    register uint64_t v = a;
 
-    asm volatile("subq %2, %1\n\t"
-                 "setc (%3)\n\t"
-                 "movq %1, %0"
-                 : "=r"(v)
-                 : "r"(a), "r"(b), "r"(c)
+    asm volatile("subq %1, %0\n\t"
+                 "setc (%2)"
+                 : "+r"(v)
+                 : "r"(b), "r"(c)
+                 : "cc");
+    return v;
+}
+
+/* v = a - b - CF */
+uint64_t sbb(const uint64_t a, const uint64_t b, uint8_t *c)
+{
+    register uint64_t v = a;
+
+    asm volatile("sbbq %1, %0\n\t"
+                 "setc (%2)"
+                 : "+r"(v)
+                 : "r"(b), "r"(c)
                  : "cc");
     return v;
 }
 
 /* (q|r) r[2] = r0 / r1 */
-int div64(uint64_t *r)
+uint8_t div64(uint64_t *r)
 {
     if (!(r[1] > 0))
         return 0;
 
     register uint64_t v1 asm("rax") = r[0];
     register uint64_t v2 asm("rdx") = 0;
-    register uint64_t v3 asm("rcx") = r[1];
+    register uint64_t v3 = r[1];
 
     asm volatile("divq %2" : "+r"(v1), "+r"(v2) : "r"(v3));
     r[0] = v1;
@@ -72,18 +82,33 @@ int div64(uint64_t *r)
 }
 
 /* (q|r) r[2] = r[2] / a; with q < 2^64 */
-int div128(uint64_t *r, const uint64_t a)
+uint8_t div128(uint64_t *r, const uint64_t a)
 {
     if (!(a > r[0]))
         return 0;
 
     register uint64_t v1 asm("rax") = r[1];
     register uint64_t v2 asm("rdx") = r[0];
-    register uint64_t v3 asm("rsi") = a;
+    register uint64_t v3 = a;
 
     asm volatile("divq %2" : "+r"(v1), "+r"(v2) : "r"(v3));
     r[0] = v1;
     r[1] = v2;
 
     return 1;
+}
+
+/* v = a bits */
+uint8_t bsr(const uint64_t a)
+{
+    register uint64_t v = 0;
+
+    asm volatile("bsrq %1, %0\n\t"
+                 "je 1f\n\t"
+                 "incq %0\n\t"
+                 "1:"
+                 : "+r"(v)
+                 : "r"(a)
+                 : "cc");
+    return (uint8_t)v;
 }
--- asmath.h
+++ asmath.h
@@ -1,15 +1,18 @@
 #include <stdint.h>
 /* ADX BMI2 */
-#define __att __attribute__((noinline, nonnull))
 /* v = a + b */
-uint64_t add(const uint64_t a, const uint64_t b, uint8_t *c) __att;
+uint64_t add(const uint64_t a, const uint64_t b, uint8_t *c);
 /* v = a + b + CF */
-uint64_t adc(const uint64_t a, const uint64_t b, uint8_t *c) __att;
+uint64_t adcx(const uint64_t a, const uint64_t b, uint8_t *c);
 /* r[2] = a * b */
-void mul(uint64_t *r, const uint64_t a, const uint64_t b) __att;
+void mulx(uint64_t *r, const uint64_t a, const uint64_t b);
 /* v = a - b */
-uint64_t sub(const uint64_t a, const uint64_t b, uint8_t *c) __att;
+uint64_t sub(const uint64_t a, const uint64_t b, uint8_t *c);
+/* v = a - b - CF */
+uint64_t sbb(const uint64_t a, const uint64_t b, uint8_t *c);
 /* (q|r) r[2] = r0 / r1 */
-int div64(uint64_t *r) __att;
+uint8_t div64(uint64_t *r);
 /* (q|r) r[2] = r[2] / a; with q < 2^64 */
-int div128(uint64_t *r, const uint64_t a) __att;
+uint8_t div128(uint64_t *r, const uint64_t a);
+/* v = a bits */
+uint8_t bsr(const uint64_t a);
