a4e40c53df7f61c5a8d6924ab654e6f0fef87dcb
Wojciech Lawren <wojciech.lawren@gmail.com>
2022-01-30 15:16:58 +0100

    rdrand

--- rdrand.c
+++ rdrand.c
@@ -4,7 +4,6 @@ uint64_t rand64()
 {
     register uint64_t v asm("rax");
     RD(RAND);
-    assert(v > 0);
     return v;
 }
 
@@ -12,7 +11,6 @@ uint32_t rand32()
 {
     register uint32_t v asm("eax");
     RD(RAND);
-    assert(v > 0);
     return v;
 }
 
@@ -20,7 +18,6 @@ uint16_t rand16()
 {
     register uint16_t v asm("ax");
     RD(RAND);
-    assert(v > 0);
     return v;
 }
 
@@ -28,7 +25,6 @@ uint64_t seed64()
 {
     register uint64_t v asm("rax");
     RD(SEED);
-    assert(v > 0);
     return v;
 }
 
@@ -36,7 +32,6 @@ uint32_t seed32()
 {
     register uint32_t v asm("eax");
     RD(SEED);
-    assert(v > 0);
     return v;
 }
 
@@ -44,12 +39,20 @@ uint16_t seed16()
 {
     register uint16_t v asm("ax");
     RD(SEED);
-    assert(v > 0);
     return v;
 }
 
-void rand_arr(uint64_t *r, const uint8_t S)
+uint8_t rand_arr(uint64_t *r, const uint16_t S)
 {
-    for (int8_t i = 0; i < S; i++)
-        r[i] = rand64();
+    for (uint16_t i = 0; i < S; i++)
+        if (!(r[i] = rand64()))
+            return 0;
+    return 1;
+}
+
+uint8_t _rand_bytes(uint8_t *r, const uint16_t S)
+{
+    if (S % 8)
+        return 0;
+    return rand_arr((uint64_t *)r, S / 8);
 }
--- rdrand.h
+++ rdrand.h
@@ -1,20 +1,30 @@
-#include <assert.h>
+/* TRNG coded by Wojciech Lawren
+
+  Copyright (C) 2021, Wojciech Lawren, All rights reserved.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #include <stdint.h>
 /* RDRAND RDSEED */
-
 #define RC 10
 
 #define RAND "rdrand %0\n\t"
 #define SEED "rdseed %0\n\t"
 
 #define RD(i)                                                                                                          \
-    asm volatile("mov %1, %%ecx\n\t"                                                                                   \
+    asm volatile("movl %1, %%ecx\n\t"                                                                                  \
                  "1:\n\t" i "jc 2f\n\t"                                                                                \
                  "loop 1b\n\t"                                                                                         \
-                 "xor %0, %0\n\t"                                                                                      \
+                 "cmovncl %%ecx, %%eax\n\t"                                                                            \
                  "2:"                                                                                                  \
                  : "=r"(v)                                                                                             \
-                 : "n"(RC)                                                                                             \
+                 : "n"((uint8_t)RC)                                                                                    \
                  : "rcx", "cc")
 
 uint64_t rand64(void);
@@ -29,4 +39,6 @@ uint32_t seed32(void);
 
 uint16_t seed16(void);
 
-void rand_arr(uint64_t *r, const uint8_t S);
+uint8_t rand_arr(uint64_t *r, const uint16_t S);
+
+uint8_t _rand_bytes(uint8_t *r, const uint16_t S);
